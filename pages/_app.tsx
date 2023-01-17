46298e98621df337de2887371c60bb688c7bdaec
Duong Vu <thien.vu97ht123@gmail.com>
2023-01-17 17:42:03 +0700

    NextJS: 03-07 Tổng quan về SWR 🎉

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
+import axiosClient from "@/api-client/axios-client";
 import { EmptyLayout } from "@/components/layout/empty";
 import { AppPropsWithLayout } from "@/models";
+import { SWRConfig } from "swr";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const Layout = Component.Layout ?? EmptyLayout;
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SWRConfig
+      value={{
+        fetcher: (url) => axiosClient.get(url),
+        shouldRetryOnError: false,
+      }}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SWRConfig>
   );
 }
