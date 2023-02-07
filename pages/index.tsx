f7a06fa082fe4cae53b83bcacdcebdc49e857bae
Duong Vu <thien.vu97ht123@gmail.com>
2023-02-07 16:24:10 +0700

    NextJS: 04-20 Hỗ trợ SEO cho trang Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Seo } from "@/components/common"
 import { FeaturedWorks, HeroSection, RecentPosts } from "@/components/home"
 import { MainLayout } from "@/components/layout"
 import { NextPageWithLayout } from "@/models"
@@ -6,6 +7,17 @@ import { Box } from "@mui/material"
 const Home: NextPageWithLayout = () => {
 	return (
 		<Box>
+			<Seo
+				data={{
+					title: "NextJS Tutorials | Easy Frontend",
+					description:
+						"Step by step tutorials to build a full CRUD website using NextJS for beginners",
+					url: "https://learn-nextjs-2023.vercel.app",
+					thumbnailUrl:
+						"https://cdn.getshifter.co/caa65008efb706a8bfc6f7e4045d6a018420c3df/uploads/2020/11/nextjs.png",
+				}}
+			/>
+
 			<HeroSection />
 			<RecentPosts />
 			<FeaturedWorks />
