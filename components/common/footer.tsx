7aeffcd3cd95d255ee962b31a02aa98c8cc6fa30
Duong Vu <thien.vu97ht123@gmail.com>
2023-02-07 16:05:08 +0700

    NextJS: 04-19 Làm giao diện footer

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,12 +1,34 @@
-import { Box } from "@mui/material"
-import * as React from "react"
+import { Facebook, GitHub, Instagram, LinkedIn } from "@mui/icons-material"
+import { Box, Icon, Stack, Typography } from "@mui/material"
 
 export interface FooterProps {}
 
 export function Footer(props: FooterProps) {
+	const socialLinks = [
+		{ icon: Facebook, url: "https://google.com" },
+		{ icon: Instagram, url: "https://google.com" },
+		{ icon: GitHub, url: "https://google.com" },
+		{ icon: LinkedIn, url: "https://google.com" },
+	]
+
 	return (
 		<Box component="footer" py={2} textAlign="center">
-			Footer
+			<Stack direction="row" justifyContent="center">
+				{socialLinks.map((item, idx) => (
+					<Box
+						key={idx}
+						component="a"
+						p={2}
+						href={item.url}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<Icon component={item.icon} sx={{ fontSize: 48 }} />
+					</Box>
+				))}
+			</Stack>
+
+			<Typography>Copyright ©{new Date().getFullYear()} All rights reserved</Typography>
 		</Box>
 	)
 }
