d01c554d7a33b956bb57796f5bc7ecbdca416bdd
Duong Vu <thien.vu97ht123@gmail.com>
2023-02-07 13:28:53 +0700

    NextJS: 04-17 Featured works layout

--- components/home/index.ts
+++ components/home/index.ts
@@ -1,2 +1,3 @@
 export * from "./hero"
 export * from "./recent-post"
+export * from "./featured-works"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { HeroSection, RecentPosts } from "@/components/home"
+import { FeaturedWorks, HeroSection, RecentPosts } from "@/components/home"
 import { MainLayout } from "@/components/layout"
 import { NextPageWithLayout } from "@/models"
 import { Box } from "@mui/material"
@@ -8,6 +8,7 @@ const Home: NextPageWithLayout = () => {
 		<Box>
 			<HeroSection />
 			<RecentPosts />
+			<FeaturedWorks />
 		</Box>
 	)
 }
