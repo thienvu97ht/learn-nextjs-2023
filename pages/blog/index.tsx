c5412219c14af68463071ea34779a9eebcf92865
Duong Vu <thien.vu97ht123@gmail.com>
2023-02-08 13:36:24 +0700

    NextJS: 05-06 Render blog list

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,5 +1,8 @@
+import { PostItem } from "@/components/blog"
+import { MainLayout } from "@/components/layout"
 import { Post } from "@/models"
 import { getPostList } from "@/utils"
+import { Box, Container, Divider } from "@mui/material"
 import { GetStaticProps } from "next"
 import Link from "next/link"
 
@@ -10,19 +13,29 @@ export interface BlogListPageProps {
 export default function BLogListPage({ posts }: BlogListPageProps) {
 	console.log("🏆 ~ BLogListPage ~ posts", posts)
 	return (
-		<div>
-			<h1>Blog List Page</h1>
+		<Box>
+			<Container>
+				<h1>Blog</h1>
 
-			<ul>
-				{posts.map((post) => (
-					<li key={post.id}>
-						<Link href={`/posts/${post.id}`}>{post.title}</Link>
-					</li>
-				))}
-			</ul>
-		</div>
+				<Box component="ul" sx={{ listStyleType: "none", p: 0 }}>
+					{posts.map((post) => (
+						<li key={post.id}>
+							<Link href={`/blog/${post.slug}`} passHref legacyBehavior>
+								<a>
+									<PostItem post={post} />
+								</a>
+							</Link>
+
+							<Divider sx={{ my: 3 }} />
+						</li>
+					))}
+				</Box>
+			</Container>
+		</Box>
 	)
 }
+
+BLogListPage.Layout = MainLayout
 
 export const getStaticProps: GetStaticProps<BlogListPageProps> = async () => {
 	// convert markdown files into list of javascript objects
